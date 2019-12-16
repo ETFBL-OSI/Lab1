8b59560e0a0012201e0ab5146438ca0b66e53312
ETF-Student1 <etf1@msil.com>
2019-12-16 10:43:59 +0100

    Funkcija za stepenovanje

---
+++ osiLab1/Stepenovanje.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+                 double stepenovanje(double a,int stepen)
+                 {
+                     return pow(a,stepen);
+                 }
+
+      
