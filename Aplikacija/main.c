9a3d1da6c2420b5dbcbb13f35335d1cb85c897d5
PC10\Student <student@etfbl.net>
2024-11-11 12:36:14 +0100

    korijen

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+float korijen(float n){
+    return (float)sqrt(n);
+}
 
 int main()
 {
+    const float n = 625;
+    printf("%.1f\n", sqrt(n));
     printf("Hello world!\n");
     return 0;
 }
