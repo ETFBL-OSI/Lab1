7fe35db0ba5ce7215bca4aca97c42a95d17a1fa8
PC12\Student <student.etfbl.net>
2024-11-11 12:42:00 +0100

    Mnozenje n brojeva

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int mnozenjenbrojeva(int n){
+    int brojevi[n];
+    int proizvod=1;
+for(int i=0;i<n;i++){
+    printf("Unesite %d. broj:",i);
+    scanf("%d",&brojevi[i]);
+}
+for(int i=0;i<n;i++){
+    proizvod=proizvod*brojevi[i];
+}
+    printf("Proizvod %d brojeva je:%d",n,proizvod);
+return proizvod;
+}
 int main()
 {
+    mnozenjenbrojeva(5);
     printf("Hello world!\n");
     return 0;
 }
