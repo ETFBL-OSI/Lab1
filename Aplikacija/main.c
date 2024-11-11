56345c6b2396e06edc32076f64aca48d79e55e31
PC04\Student <studentemail@email.com>
2024-11-11 12:43:10 +0100

    Dodata funkcija za sabiranje n brojeva.

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int sabiranje_n_brojeva(int n)
+{
+    int k = 0, rez = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        printf("Unesite %d. broj: ", i + 1);
+        scanf("%d", &k);
+
+        rez += k;
+    }
+
+    return rez;
+}
+
 int main()
 {
     printf("Hello world!\n");
+
+    printf("Unesite n: ");
+    int n;
+
+    do
+        scanf("%d", &n);
+    while(n < 1);
+
+    printf("%d", sabiranje_n_brojeva(n));
+
+
     return 0;
+
+
 }
