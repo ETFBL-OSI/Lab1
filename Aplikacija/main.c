3387cc748b7068e89769fc508b5c1680784f6d75
PC21\Student <studentetfbl.net>
2024-11-11 12:49:00 +0100

    Oduzimanje prvih n brojeva

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int oduzimanje_n_brojeva(int n)
+{
+    int razlika = 0;
+    for (int i = 0; i < n; i++)
+    {
+        razlika -= i;
+    }
+    return razlika;
+}
 int main()
 {
-    printf("Hello world!\n");
+    int n;
+    printf("Unesite n:\n");
+    scanf("%d", &n);
+    int razlika = oduzimanje_n_brojeva(n);
+    printf("Razlika: %d", razlika);
     return 0;
 }
