b9824f6e5c6862d447cd6182d0367819e6beb38d
PC03\Student <studentetfbl@etf.bl.net>
2024-11-11 12:47:01 +0100

    procenat broja

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -2,16 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
-<<<<<<< HEAD
 float korijen(float n){
     return (float)sqrt(n);
 }
 
-int main()
-{
-    const float n = 625;
-    printf("%.1f\n", sqrt(n));
-=======
 float korijen(const float n){
     return (float)sqrt(n);
 }
@@ -39,11 +33,12 @@ int sabiranje_prvih_n(int n) {
 
 int main()
 {
+    const float n = 625;
+    printf("%.1f\n", sqrt(n));
     printf("Procenat broja je: %.2lf ", procenat_broja(10,50));
     const float toSqrt = 625;
     printf("Korijen broja %.1f: %.1f\n", toSqrt, sqrt(toSqrt));
     printf("%lf\n", oduzimanje2Broja(4, 3));
->>>>>>> 926673b28713fc6565e144a483c15a00a6965e66
     printf("Hello world!\n");
     return 0;
 }
