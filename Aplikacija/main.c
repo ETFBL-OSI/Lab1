66963ebbc513d8a250b7309a21fabc1cba6e31da
PC10\Student <student@etfbl.net>
2024-11-11 12:40:07 +0100

    korijen

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+float korijen(const float n){
+    return (float)sqrt(n);
+}
 
 int sabiranje_prvih_n(int n) {
     return (n + 1) * n / 2;
@@ -7,6 +12,8 @@ int sabiranje_prvih_n(int n) {
 
 int main()
 {
+    const float toSqrt = 625;
+    printf("Korijen broja %.1f: %.1f\n", toSqrt, sqrt(toSqrt));
     printf("Hello world!\n");
     return 0;
 }
