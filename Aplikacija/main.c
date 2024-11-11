ba41fed0bdf61bdb91b03f08dd85430766b1d864
PC03\Student <studentetfbl@etf.bl.net>
2024-11-11 12:38:51 +0100

    procenat broja

--- Aplikacija/main.c
+++ Aplikacija/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double procenat_broja(double broj, double procenat){
+    return broj * procenat / (double)100;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("Procenat broja je: %.2lf ", procenat_broja(10,50));
     return 0;
 }
